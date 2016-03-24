1baf1819df8c00cf420f20acb0ca131787a32867
vladSumy <gvvlad@mail.ru>
2016-03-24 16:53:26 +0200

    create api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+class ApplicationController < ActionController::API
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
+  def render_resource_data(resource, options = {})
+    render json: options.merge({ data: resource})
+  end
 end
