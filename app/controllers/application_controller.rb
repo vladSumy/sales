20c708ff068902f2191523ccf95d3019e8f5df22
vladSumy <gvvlad@mail.ru>
2016-03-26 22:10:55 +0200

    create api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::API
-  after_filter :set_access_control_headers
+  # after_filter :set_access_control_headers
+  # def set_access_control_headers
+  #  headers['Access-Control-Allow-Origin'] = '*'
+  #  headers['Access-Control-Request-Method'] = '*'
+  # end
 
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Request-Method'] = '*'
-  end
   def render_resource_data(resource, options = {})
     render json: options.merge({ data: resource})
   end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,18 @@ class ArticlesController < ApplicationController
     @all = Article.all
     render_resource_data(@all)
   end
+  def index
+    @all = Article.all
+    render_resource_data(@all)
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    render_resource_data(@article)
+  end
+  private
+  def article_params
+    params.require(:article).permit(:id, :title, :text)
+  end
 end
+
